8f23c630b7ac8db85b21391174d17fa1e06e7962
Ben Titcomb <btitcomb@scpr.org>
2015-09-02 19:21:28 -0700

    regex sucks

--- map.js
+++ map.js
@@ -37,7 +37,8 @@ initializeMap = function(){
       var text = kmlEvent.featureData.description
       $("#fullcirclemap").animate({width: "60%"}, 1000, function(){
         $("#charm").css("width", "40%");
-        var videoId = text.match(/\?v=(.*)"/);
+        var matches = text.match(/(youtu.be\/|youtube.com\/(watch\?(.*&)?v=|(embed|v)\/))([^\?&\"\'>]+)/);
+        var videoId = matches[matches.length - 1]
         var embed = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe>'
         $("#charm").html(embed);
       });
